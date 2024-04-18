5f5bbe24a4b0fd4eb80bf57befc55bf871853116
zhufucdev <1095291434@qq.com>
2024-04-18 18:05:21 +0800

    :pencil2: usripaddr

--- src/main/java/com/javamasters/model/LoginRequest.java
+++ src/main/java/com/javamasters/model/LoginRequest.java
@@ -1,5 +1,5 @@
 package com.javamasters.model;
 
 public record LoginRequest(String username, String password, String ifautologin, String channel, String pagesign,
-                           String useripadd) {
+                           String usripadd) {
 }
