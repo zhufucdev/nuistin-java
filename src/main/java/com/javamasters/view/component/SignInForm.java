c6befe53a1338752ac8649918b304f30966c913a
zhufucdev <1095291434@qq.com>
2024-04-21 19:59:38 +0800

    :sparkles: autofill

--- src/main/java/com/javamasters/view/component/SignInForm.java
+++ src/main/java/com/javamasters/view/component/SignInForm.java
@@ -9,12 +9,20 @@ import io.reactivex.rxjava3.subjects.BehaviorSubject;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.FocusAdapter;
+import java.awt.event.FocusEvent;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
 
 public class SignInForm extends Container {
+    private final KeychainViewModel keychain;
     private final Choice ispDropdown = new Choice();
+    private final Labeled<TextField> usernameLabel;
+    private final Labeled<JPasswordField> passwordLabel;
+
     public final BehaviorSubject<String> username = BehaviorSubject.create(), password = BehaviorSubject.create();
     public final BehaviorSubject<ISP> isp = BehaviorSubject.create();
 
@@ -23,19 +31,21 @@ public class SignInForm extends Container {
     private final Map<String, ISP> namedIsps = new HashMap<>();
 
     public SignInForm(KeychainViewModel keychain, Resources resources) {
-        var usernameLabel = new Labeled<>(new TextField()) {
+        this.keychain = keychain;
+
+        usernameLabel = new Labeled<>(new TextField()) {
             @Override
             public Insets getInsets() {
                 return new Insets(0, 4, 10, 6);
             }
         };
-        add(usernameLabel);
-        var passwordLabel = new Labeled<>(new JPasswordField()) {
+        passwordLabel = new Labeled<>(new JPasswordField()) {
             @Override
             public Insets getInsets() {
                 return new Insets(0, 4, 18, 6);
             }
         };
+        add(usernameLabel);
         add(passwordLabel);
         add(ispDropdown);
         setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
@@ -50,8 +60,14 @@ public class SignInForm extends Container {
             var next = namedIsps.get((String) e.getItem());
             isp.onNext(next);
         });
+        disposables.add(
+                isp.subscribe(isp -> namedIsps.entrySet().stream()
+                        .filter(pair -> pair.getValue().equals(isp))
+                        .findFirst()
+                        .ifPresent(next -> ispDropdown.select(next.getKey())))
+        );
 
-        disposables.addAll(
+        disposables.add(
                 resources.getTranslation().subscribe(t -> {
                     ispDropdown.removeAll();
                     for (int i = 0; i < ISP.values().length; i++) {
@@ -60,21 +76,56 @@ public class SignInForm extends Container {
                         ispDropdown.add(name);
                         namedIsps.put(name, isp);
                     }
-                }),
-                keychain.getAccountIds()
-                        .flatMap(saved -> username.map(input ->
-                                input.isEmpty() ? Optional.<String>empty() :
+                })
+        );
+
+        setupAutofill();
+    }
+
+    private void setupAutofill() {
+        disposables.add(keychain.getAccountIds()
+                .flatMap(saved -> username.map(input ->
+                        input.isEmpty() ? Optional.<String>empty() :
                                 saved.stream().filter(id -> id.startsWith(input) && !id.equals(input))
                                         .sorted()
                                         .findFirst())
-                        )
-                        .subscribe(potential -> {
-                            if (potential.isPresent()) {
-                                var inputTail = usernameLabel.field.getText().length();
-                                usernameLabel.field.setText(potential.get());
-                                usernameLabel.field.setSelectionStart(inputTail);
-                                usernameLabel.field.setSelectionEnd(potential.get().length());
-                            }
+                )
+                .subscribe(potential -> {
+                    if (potential.isPresent()) {
+                        var inputTail = usernameLabel.field.getText().length();
+                        usernameLabel.field.setText(potential.get());
+                        usernameLabel.field.setSelectionStart(inputTail);
+                        usernameLabel.field.setSelectionEnd(potential.get().length());
+                    }
+                })
+        );
+        usernameLabel.field.addFocusListener(new FocusAdapter() {
+            @Override
+            public void focusLost(FocusEvent e) {
+                tryAutofill(usernameLabel.field.getText());
+            }
+        });
+        usernameLabel.field.addActionListener(e -> {
+            tryAutofill(usernameLabel.field.getText());
+            passwordLabel.field.grabFocus();
+        });
+        usernameLabel.field.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyTyped(KeyEvent e) {
+                if (e.getKeyCode() == KeyEvent.VK_TAB) {
+                    passwordLabel.field.grabFocus();
+                }
+            }
+        });
+    }
+
+    private void tryAutofill(String username) {
+        disposables.add(
+                keychain.getAccount(username)
+                        .onErrorComplete()
+                        .subscribe(acc -> {
+                            password.onNext(acc.password());
+                            isp.onNext(acc.isp());
                         })
         );
     }
